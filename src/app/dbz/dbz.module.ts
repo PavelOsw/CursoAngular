23178de839b4222cc8d478973372e5add04713b6
PavelOsw <pavel.osw117@gmail.com>
2023-08-28 19:16:28 -0600

    Fin sección 5

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MainPageComponent } from './pages/main-pages.component';
+import { ListComponent } from './components/list/list.component';
+import { AddcharacterComponent } from './components/addcharacter/addcharacter.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainPageComponent,
+    ListComponent,
+    AddcharacterComponent
+  ],
+  exports: [
+    MainPageComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class DbzModule { }
